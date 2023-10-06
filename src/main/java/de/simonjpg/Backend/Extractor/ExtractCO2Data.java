ea8e3987c055a16bcc5eb28ef845b9bbe9528679
SimonJpg <simonbalcke3@gmail.com>
2023-10-06 16:46:34 +0200

    Add features to extract CO2 data

--- src/main/java/de/simonjpg/Backend/Extractor/ExtractCO2Data.java
+++ src/main/java/de/simonjpg/Backend/Extractor/ExtractCO2Data.java
@@ -22,8 +22,9 @@ public class ExtractCO2Data {
     /**
      * Method extractData.
      * <p>
-     *     Extracts CO2 data for a given year.
+     * Extracts CO2 data for a given year.
      * </p>
+     *
      * @param year the year of the CO2 data to be filtered for.
      * @return a {@link java.util.List} with the CO2 data.
      */
@@ -31,21 +32,21 @@ public class ExtractCO2Data {
         List<Double> extractedData = new ArrayList<>();
         Map<Integer, List<String>> rawData = XLSXCrawler.instance().getCO2Data();
         LOGGER.info("Extracting CO2 data of year {}", year);
-        for (int i = 0; i < rawData.size(); i++) {
 
+        for (int i = 0; i < rawData.size(); i++) {
             /*
-            * The rawData contains every Cell of the "Daten" Sheet of the .xlsx file.
-            * To extract the data, a special pattern is needed to be followed:
-            * First we check, if the ArrayList of the current iteration is empty to avoid an IndexOutOfBoundsException.
-            * Second we check for the value of the ArrayList if it matches with the searched year.
-            * If so, we know that the current row contains a part of our CO2 Data.
-            * If we look at the complete value of the raw data, we see, that after the year, 6 CO2 values follow.
-            * The same with the next rows except for the years 2020 and 2022.
-            *
-            * In year 2020 the pattern is 6 on the current row and 6 on the following row.
-            * In year 2022 the pattern is only 6 on the current row.
-            *
-            * Now that we know how to filter for the data we can extract them.
+             * The rawData contains every Cell of the "Daten" Sheet of the .xlsx file.
+             * To extract the data, a special pattern is needed to be followed:
+             * First we check, if the ArrayList of the current iteration is empty to avoid an IndexOutOfBoundsException.
+             * Second we check for the value of the ArrayList if it matches with the searched year.
+             * If so, we know that the current row is a part of our CO2 Data.
+             * If we look at the complete value of the raw data, we see, that after the year, 6 CO2 values follow.
+             * The same with the next 4 rows (5 in total) except for the years 2020 and 2022.
+             *
+             * In year 2020 the pattern is 6 on the current row and 6 on the following row. (2 rows in total)
+             * In year 2022 the pattern is only 6 on the current row. (1 row in total)
+             *
+             * Now that we know how to filter for the data we can extract them.
              */
 
             if (!rawData.get(i).isEmpty() && rawData.get(i).get(0).equals(year)) {
@@ -97,5 +98,101 @@ public class ExtractCO2Data {
         }
         return extractedData;
     }
-    //TODO: Add methods for 2020 and 2022
+
+    /**
+     * Method extractDataOf2020.
+     * <p>
+     * Extracts CO2 data for the year 2020.
+     * </p>
+     *
+     * @return a {@link java.util.List} with the CO2 data.
+     */
+
+    public List<Double> extractDataOf2020() {
+        List<Double> extractedData = new ArrayList<>();
+        Map<Integer, List<String>> rawData = XLSXCrawler.instance().getCO2Data();
+        LOGGER.info("Extracting CO2 data of year 2020");
+
+        for (int i = 0; i < rawData.size(); i++) {
+            /*
+             * The rawData contains every Cell of the "Daten" Sheet of the .xlsx file.
+             * To extract the data, a special pattern is needed to be followed:
+             * First we check, if the ArrayList of the current iteration is empty to avoid an IndexOutOfBoundsException.
+             * Second we check for the value of the ArrayList if it matches with the searched year.
+             * If so, we know that the current row contains a part of our CO2 Data.
+             * If we look at the complete value of the raw data, we see, that after the year, 6 CO2 values follow.
+             * The same with the next rows except for the years 2020 and 2022.
+             *
+             * In year 2020 the pattern is 6 on the current row and 6 on the following row. (2 in total)
+             * In year 2022 the pattern is only 6 on the current row. (1 in total)
+             *
+             * Now that we know how to filter for the data we can extract them.
+             */
+
+            if (!rawData.get(i).isEmpty() && rawData.get(i).get(0).equals("2020.0")) {
+                LOGGER.debug("Extract data of {}. element of the HashMap", i);
+                // first row (row with the year of the values)
+                extractedData.add(Double.parseDouble(rawData.get(i).get(1)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(2)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(3)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(4)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(5)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(6)));
+
+                LOGGER.debug("Extract data of {}. element of the HashMap", i + 1);
+                // second row
+                extractedData.add(Double.parseDouble(rawData.get(i + 1).get(0)));
+                extractedData.add(Double.parseDouble(rawData.get(i + 1).get(1)));
+                extractedData.add(Double.parseDouble(rawData.get(i + 1).get(2)));
+                extractedData.add(Double.parseDouble(rawData.get(i + 1).get(3)));
+                extractedData.add(Double.parseDouble(rawData.get(i + 1).get(4)));
+                extractedData.add(Double.parseDouble(rawData.get(i + 1).get(5)));
+            }
+        }
+        return extractedData;
+    }
+
+    /**
+     * Method extractDataOf2022.
+     * <p>
+     * Extracts CO2 data for the year 2022.
+     * </p>
+     *
+     * @return a {@link java.util.List} with the CO2 data.
+     */
+
+    public List<Double> extractDataOf2022() {
+        List<Double> extractedData = new ArrayList<>();
+        Map<Integer, List<String>> rawData = XLSXCrawler.instance().getCO2Data();
+        LOGGER.info("Extracting CO2 data of year 2022");
+
+        for (int i = 0; i < rawData.size(); i++) {
+            /*
+             * The rawData contains every Cell of the "Daten" Sheet of the .xlsx file.
+             * To extract the data, a special pattern is needed to be followed:
+             * First we check, if the ArrayList of the current iteration is empty to avoid an IndexOutOfBoundsException.
+             * Second we check for the value of the ArrayList if it matches with the searched year.
+             * If so, we know that the current row contains a part of our CO2 Data.
+             * If we look at the complete value of the raw data, we see, that after the year, 6 CO2 values follow.
+             * The same with the next rows except for the years 2020 and 2022.
+             *
+             * In year 2020 the pattern is 6 on the current row and 6 on the following row. (2 in total)
+             * In year 2022 the pattern is only 6 on the current row. (1 in total)
+             *
+             * Now that we know how to filter for the data we can extract them.
+             */
+
+            if (!rawData.get(i).isEmpty() && rawData.get(i).get(0).equals("2022.0")) {
+                LOGGER.debug("Extract data of {}. element of the HashMap", i);
+                // first row (row with the year of the values)
+                extractedData.add(Double.parseDouble(rawData.get(i).get(1)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(2)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(3)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(4)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(5)));
+                extractedData.add(Double.parseDouble(rawData.get(i).get(6)));
+            }
+        }
+        return extractedData;
+    }
 }
