f327df6ec08e0bf8e64366e48ea0324fd1914448
SimonJpg <simonbalcke3@protonmail.com>
2023-10-26 14:18:29 +0200

    Add Javadoc

--- src/main/java/de/simonjpg/Frontend/UI/Swing/SearchText.java
+++ src/main/java/de/simonjpg/Frontend/UI/Swing/SearchText.java
@@ -9,19 +9,30 @@ import java.awt.RenderingHints;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-// TODO: Add Javadoc
+/**
+ * Class SearchText.
+ * <p>
+ *     Search text field on top of the JFrame.
+ * </p>
+ * @author SimonJpg
+ * @see javax.swing.JTextField
+ */
 public class SearchText extends JTextField {
-    private final String hint = "Search here ...";
 
+    /**
+     * Constructor SearchText.
+     * <p>
+     *     sets border around the search text field
+     * </p>
+     */
     public SearchText() {
         setBorder(new EmptyBorder(5, 5, 5, 5));
         setSelectionColor(new Color(220, 204, 182));
     }
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (getText().length() == 0) {
+        if (getText().isEmpty()) {
             int h = getHeight();
             Graphics2D g2 = (Graphics2D) g;
             g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
@@ -30,9 +41,11 @@ public class SearchText extends JTextField {
             int c0 = getBackground().getRGB();
             int c1 = getForeground().getRGB();
             int m = 0xfefefefe;
+
+            // >>> unsigned right shift operator
             int c2 = ((c0 & m) >>> 1) + ((c1 & m) >>> 1);
             g2.setColor(new Color(c2, true));
-            g2.drawString(hint, ins.left, h / 2 + fm.getAscent() / 2 - 2);
+            g2.drawString("Search here ...", ins.left, h / 2 + fm.getAscent() / 2 - 2);
         }
     }
 }
