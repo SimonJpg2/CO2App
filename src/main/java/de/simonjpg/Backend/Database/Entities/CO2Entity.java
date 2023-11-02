0769ec4cdb8788443b5ff6533b9580c41c89f648
SimonJpg <simonbalcke3@protonmail.com>
2023-11-02 16:10:31 +0100

    Add javadoc

--- src/main/java/de/simonjpg/Backend/Database/Entities/CO2Entity.java
+++ src/main/java/de/simonjpg/Backend/Database/Entities/CO2Entity.java
@@ -1,9 +1,27 @@
 package de.simonjpg.Backend.Database.Entities;
-// TODO: Add javadoc
+
+/**
+ * Class Co2Entity.
+ * <p>
+ *     Class to represent the data of the database.
+ * </p>
+ * @author Simon Balcke
+ */
+
 public class CO2Entity {
     private int id;
     private final int year;
     private final double value;
+
+    /**
+     * Constructor Co2Entity.
+     * <p>
+     *     Creates new instance
+     * </p>
+     * @param id id of the database entry
+     * @param year year of the co2 value
+     * @param value actual co2 data
+     */
 
     public CO2Entity(int id, int year, double value) {
         this.id = id;
@@ -11,11 +29,21 @@ public class CO2Entity {
         this.value = value;
     }
 
+    /**
+     * Constructor Co2Entity.
+     * <p>
+     *     Creates new instance.
+     * </p>
+     * @param year year of the co2 value
+     * @param value actual co2 data
+     */
+
     public CO2Entity(int year, double value) {
         this.year = year;
         this.value = value;
     }
 
+    @SuppressWarnings("unused")
     public CO2Entity() {
         this.id = -1;
         this.year = -1;
@@ -30,6 +58,7 @@ public class CO2Entity {
         return value;
     }
 
+    @SuppressWarnings("unused")
     public int getId() {
         return id;
     }
