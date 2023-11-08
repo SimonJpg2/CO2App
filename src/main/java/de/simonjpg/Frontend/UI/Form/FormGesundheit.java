e03943ad502b4c3e15a800caeacc8bc02db4d6a0
SimonJpg <simonbalcke3@protonmail.com>
2023-11-08 17:07:04 +0100

    Add feature to display harms to humans caused by CO2

--- src/main/java/de/simonjpg/Frontend/UI/Form/FormGesundheit.java
+++ src/main/java/de/simonjpg/Frontend/UI/Form/FormGesundheit.java
@@ -1,5 +1,30 @@
 package de.simonjpg.Frontend.UI.Form;
 
+import de.simonjpg.Frontend.UI.CurveLineChart.raven.chart.ModelChart;
+
+/**
+ * Class FormGesundheit.
+ * <p>
+ *     Die CO² Konzentration steigt pro jahr um ca. 2ppm.
+ *     Es dauert hunderte Jahre, bis die Luft der Atmosphäre als verbraucht gilt (1400ppm).
+ *     Es dauert 40.000 Jahre, bis die Luft der Atmosphäre tödlich für den Menschen werden würde (80000ppm).
+ * </p>
+ * <p>
+ *     Das größere Problem ist der Treibhauseffekt. Bevor wir aufgrund der Co² Konzentration "ersticken", zerstören wir
+ *     unseren Planeten, welcher für uns Menschen die Lebensgrundlage darstellt.
+ * </p>
+ * @author Simon Balcke
+ * @see de.simonjpg.Frontend.UI.Form.FormDashboard
+ */
+
 public class FormGesundheit extends FormDashboard {
-    //TODO: implement features for displaying the health aspect
+    @Override
+    public void setData() {
+        chart.setTitle("Es wird jahrtausende dauern bis CO² unsere Gesundheit gefährdet.");
+        for (int i = 400; i < 1400; i++) {
+            chart.addData(new ModelChart("", new double[]{i}));
+        }
+        chart.start();
+        chart.setFillColor(true);
+    }
 }
